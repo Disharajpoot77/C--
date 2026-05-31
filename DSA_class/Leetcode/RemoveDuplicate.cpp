6aa864ff7ca85e83ac51339bad1baac049d43e43
Disha rajpoot <rajpootdisha42@gmail.com>
2026-05-31 23:01:20 +0530

    duplicate

---
+++ DSA_class/Leetcode/RemoveDuplicate.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	set<int> st;
+	for(int i=0;i<n;i++){
+		st.insert(arr[i]);
+	}
+	    for(auto x : st) {
+        cout << x << " ";
+    }
+
+
+}
