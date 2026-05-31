a23a03c4b07893d1a45de3a94893f4f52f9ca93e
Disha rajpoot <rajpootdisha42@gmail.com>
2026-05-31 23:01:56 +0530

    longestconsecutive

---
+++ DSA_class/Striver/LogestConsecutive.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+
+	sort(arr,arr+n);
+	int count=0;
+	int L=0;
+	for(int i=0; i<n; i++){
+		cout<<"c"<<count<<endl;
+		cout<<"l"<<L<<endl;
+		if(arr[i]==0 && i==0){
+			count++;
+		}
+		if(arr[i]==arr[L+1]){
+			count++;
+			L=arr[i];
+		}
+		else if(arr[i]!=arr[L+1]){
+			break;
+		}
+		else{
+			i++;
+		}
+	}
+
+	cout<<count-1;
+	return 0;
+
+}
