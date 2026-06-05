c405aac16ff24295c48c39e5497011ea25c91270
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-05 21:00:58 +0530

    zero

--- DSA_class/Striver/SetMatrixZeroes.cpp
+++ DSA_class/Striver/SetMatrixZeroes.cpp
@@ -6,11 +6,39 @@ int main(){
 	int arr[n][m];
 	  for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin  >> arr[i][j];
+            cin >> arr[i][j];
         }
     }
 
+	int record[n*m][2];
+	int r=0;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if(arr[i][j]==0){
+				record[r][0]=i;
+				record[r][1]=j;
+				r++;
+			}
+		}
+	}
+	
+	for(int i=0; i<r; i++){
+		for(int j=0; j<m; j++){
+			arr[record[i][0]][j]=0;
+		}
+	
+	for(int j=0; j<n; j++){
+			arr[j][record[i][1]]=0;
+		}
 
+	}
+
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 
 
 }
