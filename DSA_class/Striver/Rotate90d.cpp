37f5739aabc92eca902a3133773fea380bc744fb
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-05 21:04:42 +0530

    correct

--- DSA_class/Striver/Rotate90d.cpp
+++ DSA_class/Striver/Rotate90d.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
 	int n;
@@ -12,10 +13,13 @@ int main(){
 
 	for(int i=0;i<n-1;i++){
 		for(int j=0;j<n;j++){
-			swap(arr[i][j],arr[j][i]);
-			
+			swap(arr[i][j],arr[j][i]);	
 		}
 		
+	}
+
+	for(int i=0; i<n;i++){
+		reverse(arr[i].begin(),arr[i].end());
 	}
 
 }
