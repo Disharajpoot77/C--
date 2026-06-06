e0238f99f80e26733b90f345d1c3264a8fc67d33
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-06 14:04:31 +0530

    nums

---
+++ DSA_class/Striver/missingduplicate.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int> nums(n);
+	for(int i=0; i<n; i++){
+		cin>>nums[i];
+	}
+        sort(nums.begin(), nums.end());
+
+		 for(int i=1; i<n; i++){
+            if(nums[i]==nums[i-1]){
+            cout<<nums[i]<<" ";
+            }
+        }
+
+         int temp=nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]==temp+1){
+                temp=nums[i];
+            }
+            else{
+              break;
+            }
+        }
+		cout<<temp+1;
+       
+}
