f134b817e69b52c55d2ce1d4d18c23cb07f4af75
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-06 21:10:24 +0530

    2D

---
+++ DSA_class/Striver/searchin2Dmatrix.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+        int m=matrix.size();
+        int n=matrix[0].size();
+        bool find=false;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==target){
+                    find=true;
+                }  
+            }
+        }
+        c find;
+    }
