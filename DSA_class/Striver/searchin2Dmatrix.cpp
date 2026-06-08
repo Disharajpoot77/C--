cc69d8cb1766d155cb702750d2fa9e8e08385fc3
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-08 13:59:35 +0530

    2darr

--- DSA_class/Striver/searchin2Dmatrix.cpp
+++ DSA_class/Striver/searchin2Dmatrix.cpp
@@ -1,17 +1,27 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-	
+    int n;
+    cin>>n;
+        int target;
+        cin>>target;
+        vector<vector<int>> matrix(n, vector<int>(n));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cin>>matrix[i][j];
+            }
+        }
         int m=matrix.size();
-        int n=matrix[0].size();
+        int x=matrix[0].size();
         bool find=false;
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<x; j++){
                 if(matrix[i][j]==target){
                     find=true;
                 }  
             }
         }
-        c find;
+        cout<<find;
     }
