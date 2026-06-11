215d6ae3e4485303172c216c1b27661d4ab20a2a
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-11 19:34:00 +0530

    loop

---
+++ DSA_class/Striver/Detectloo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+	// input linked list
+	int n;
+	cin>>n;
+	int data;
+	cin>>data;
+	 map<ListNode*,int>mpp;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(mpp.find(temp)!=mpp.end()){
+                return true;
+            }
+            mpp[temp]=1;
+            temp=temp->next;
+        }
+	
+}
