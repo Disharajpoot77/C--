9b2745bdca3cdfd2110f51f3bc3f13165f336b60
Disha rajpoot <rajpootdisha42@gmail.com>
2026-06-15 13:40:15 +0530

    array

---
+++ DSA_class/Striver/NmeetingInONEroom.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int start[n];
+	for(int i=0;i<n;i++){
+		cin>>start[i];
+	}
+	int end[n];
+	for(int i=0;i<n;i++){
+		cin>>end[i];
+	}
+	 int cnt=0;
+        int compare=end[0];
+        for(int i=0; i<n; i++){
+            if(i==0){
+                cnt++;
+            }
+            else if(start[i]>compare){
+                cnt++;
+                compare=end[i];
+            }
+        }
+		cout<<cnt;
+	
+	return 0;
+}
